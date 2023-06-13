038586b04a97ad39fa20a844fe83ea5b5a6433a5
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-13 09:36:56 +0200

    Create connexion.php
    Création du fichier
    Création du formulaire

---
+++ connexion.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    <header>
+        <nav></nav>
+    </header>
+    <main>
+    <form action="" method="post">
+            <fieldset> 
+            <legend>Formulaire de connexion</legend>
+                        
+            <label for="login">Log In :</label>
+            <input type="text" name="login" id="login"><br>
+            
+            <label for="pword">Mot de passe :</label>
+            <input type="password" name="password" id="pword"><br>
+            
+            <input type="reset" value="Effacer"><br>
+            <input type="submit" value="Connexion">
+            </fieldset>
+        </form>
+    </main>
+    <footer>
+
+    </footer>
+    
+
+
+
+</body>
+</html>
