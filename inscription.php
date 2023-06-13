55ec7b06fa4fc00dc90174bd76be14323b74404c
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-13 13:03:01 +0200

    Update
    Récupération des valeurs des champs dans le formulaire et insertion de ces valeurs dans la table "utilisateurs"

--- inscription.php
+++ inscription.php
@@ -15,7 +15,7 @@
         <nav></nav>
     </header>
     <main>
-        <form action="<?php $_SERVER["PHP_SELF"] ?>" method="post" enctype="application/x-www-form-urlencoded">
+        <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post" enctype="application/x-www-form-urlencoded">
             <fieldset>
                 <legend>Formulaire d'inscription</legend>
                 <table>
@@ -83,19 +83,72 @@
             </fieldset>
         </form>
         <?php
-        if (isset(  $_POST["prenom"]) && $_POST["nom"] && $_POST["login"] && $_POST["password"])
+
+        // Inclusion du fichier functions.php
+        include("./includes/functions.php");
+
+        // Connexion au serveur
+        $idcom=connexpdo("moduleconnexion","myparam");
+
+        if (!empty($_POST["prenom"]) && !empty($_POST["nom"]) && !empty($_POST["login"]) && !empty($_POST["password"])) # Vérification de l'existance des saisies obligatoires
         {
-            $prenom=$_POST["prenom"];
-            $nom=$_POST["nom"];
-            $login=$_POST["login"];
-            $pass=$_POST["password"];
-    
-            echo "<h2>" . $prenom ." ".$nom." ".$login." ".$pass."</h2>";
+            $id_utilisateurs=$idcom->lastInsertId();
+            $prenom=$idcom->quote($_POST['prenom']);
+            $nom=$idcom->quote($_POST['nom']);
+            $login=$idcom->quote($_POST['login']);
+            $password=$idcom->quote($_POST['password']);
+            // Requête SQL
+            $requete3="INSERT INTO utilisateurs VALUES($id_utilisateurs, $login, $prenom, $nom, $password)";
+            $nblignes=$idcom->exec($requete3);
+            if ($nblignes!=1)
+            {
+                $mess_erreur=$idcom->errorInfo();
+                echo "Insertion impossible, code", $idcom->errorCode(),$mess_erreur[2];
+                echo "<script type=\"text/javascript\">
+                alert('Erreur : ".$idcom->errorCode()."')</script>";
+            }
+            else
+            {
+                echo "<script type=\"text/javascript\">
+                alert('Vous êtes enregistré. Votre id est le : ".$idcom->lastInsertId()."')</script>";
+            $idcom=NULL;
+            }
+            
         }
-        // } else {
-        //     header("Location:inscription.php");
-        // }
+        else
+        {
+            echo "<h3>Formulaire à compléter !</h3>";
+        }
         
+
+        // Requête sans résultats
+        // $requete1="UPDATE `utilisateur` SET `nom`='Bremaud' WHERE `id`='1'"; # Ecriture de la requete dans la variable $requete1
+        // $nb=$idcom->exec($requete1); # Envoi de la requete "requete1" à l'aide de la méthode exec() qui renvoi le nombre de lignes affectées
+        // echo"<p>$nb ligne(s) modifiée(s)<hr></p>"; # Affichage du nombre de lignes affectées par la requete "requete1"
+
+        // Requête avec résultats
+        // $requete2="SELECT * FROM `utilisateurs`"; # Ecriture de la requete dans la variable $requete2
+        // $result=$idcom->query($requete2); # Envoi de la requete "requete2" à l'aide de la méthode query() et retourne le résultat dans la variable "$result"
+        // if (!$result) # Si "$result" = FALSE 
+        // {
+        //     $mes_erreur=$idcom->errorInfo(); # On récupère les erreurs
+        //     echo "Lecture impossible, code", $idcom->errorCode(),$mes_erreur[2]; # Puis on les affiche
+        // } 
+        // else # Sinon si "$result" est différent de FALSE
+        // {
+        //     while ($row = $result->fetch(PDO::FETCH_NUM)) # Retourne un tableau de type indicé "PDO::FETCH_NUM", peu aussi être un tableau associatif "PDO::FETCH_ASSOC"
+        //     {
+        //         foreach ($row as $value)
+        //         {
+        //             echo $value,"&nbsp;";
+        //         }
+        //         echo "<hr>";
+        //     }
+        //     $result->closeCursor(); # Permet de libérer la mémoire occupée par le résultat de la requete "requete2", très utile avant d'envoyer une NOUVELLE requete au serveur
+        // }
+        // // Fermeture de la connexion
+        // $idcom=NULL;    
+
         ?>
 
     </main>
