0ccf51e0a788fb6145ff696e0cdc6b040462c437
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-13 13:04:59 +0200

    Update functions.php
    Déclaration de la fonction "connexpdo" qui permet de se connecter à la BDD "$base" suivant les paramètres présents dans le fichier "$param"

--- includes/functions.php
+++ includes/functions.php
@@ -8,5 +8,26 @@ function isPasswordSame($pass1, $pass2)
     }
     
 }
+// Création de la fonction de connexion afin de la rappeler à notre guise
+function connexpdo($base,$param)
+{
+    // Inclusion des paramètres su serveur
+    include_once($param.".inc.php");
+    // Connexion en serveur
+    $dsn="mysql:host=".HOST.";dbname=".$base;
+    $user=USER;
+    $pass=PASS;
+    try
+    {
+        $idcom=new PDO($dsn,$user,$pass);
+        return $idcom;
+    }
+        catch(PDOException $except)
+    {
+        echo"Echec de la connexion",$except->getMessage();
+        return FALSE;
+        exit();
+    }
+}
 
 ?>
